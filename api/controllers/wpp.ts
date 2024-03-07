3de5fd2de2fb73a151cfbd6554b86fef2ca4e51c
Ivan Rodriguez <ivan@abodehr.com>
2024-03-07 11:51:16 -0500

    feat: handling unique session

--- api/controllers/wpp.ts
+++ api/controllers/wpp.ts
@@ -3,44 +3,59 @@ import QRCode from 'qrcode';
 import { Controller, Get, Response, Route } from 'tsoa';
 import { PassThrough, Readable } from 'stream';
 import { wppClient } from '../wppClient';
+import { addMinutes, getUnixTime } from 'date-fns';
+import { Sessions } from '../sessions';
 
 @Route('/wpp')
 export default class WppController extends Controller {
   //create property client
   readonly #client: Client = wppClient;
+  readonly #sessions = Sessions.getInstance();
 
   @Get('/auth')
   @Response('200', 'Success', 'sdsd')
   public async initializeWebQR() {
-    this.#client.initialize();
+    const currentSession = this.#sessions.get('sessionUnique');
+
+    if (
+      currentSession !== undefined &&
+      getUnixTime(new Date()) < currentSession.endTime
+    ) {
+      return currentSession.stream;
+    }
+
     const QRreadStream: Promise<Readable> = new Promise((resolve) => {
       const stream = new PassThrough();
-      this.#client.on('qr', async (qr) => {
-        console.log('QR received!', qr);
 
+      this.#client.on('qr', async (qr) => {
         await QRCode.toFileStream(stream, qr, {
           type: 'png',
           width: 200,
           errorCorrectionLevel: 'H'
         });
-        console.log('QR code generated!');
+
+        this.#sessions.set('sessionUnique', {
+          stream,
+          startTime: getUnixTime(new Date()),
+          endTime: getUnixTime(addMinutes(new Date(), 1))
+        });
+
         resolve(stream);
       });
 
       this.#client.on('ready', async () => {
         console.log('Client is ready!');
       });
+
+      if (this.#sessions.size() === 0) {
+        this.#client.initialize();
+      }
     });
 
     this.setHeader('Content-Type', 'image/png');
 
-    this.#client.initialize();
-
-    return QRreadStream;
+    return await QRreadStream;
   }
-
-  // @Get('/create-session')
-  // public async getSessions(): Promise<Readable> {}
 
   @Get('/get-chats')
   public async getChats() {
