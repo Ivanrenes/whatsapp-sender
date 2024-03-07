e1235e348810bcd5dafd36a307b0fe758a226099
Ivan Rodriguez <ivan@abodehr.com>
2024-03-07 09:55:37 -0500

    feat: tsoa config added

--- build/services/wpp.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.WppService = void 0;
-class WppService {
-    generateQR() { }
-}
-exports.WppService = WppService;
